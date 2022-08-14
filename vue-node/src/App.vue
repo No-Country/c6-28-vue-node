d192f24d0578bc9e35a42d6241070bd27621e136
subjekt-iv <agustinprado007@gmail.com>
2022-08-14 01:47:47 -0300

    Home View

--- vue-node/src/App.vue
+++ vue-node/src/App.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="container">
-    <router-view />
-  </div>
+  <router-view />
 </template>
 
+
+
+<script>
+export default {};
+</script>
 <style>
+#app {
+  /* font-family:'Poppins', sans-serif; */
+  box-sizing: border-box;
+  margin: 0%;
+  padding: 0%;
+}
 </style>
--- vue-node/src/main.js
+++ vue-node/src/main.js
@@ -1,14 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap.js"
+import "material-icons/iconfont/material-icons.css"
 
 
 
 
 createApp(App)
     .use(router)
-
-
-
     .mount('#app')
+
