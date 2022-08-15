357c1f9edd10d8782d7ada9308889d32defbc67b
Daniel Aguilar <dyangeltk17@gmail.com>
2022-08-15 08:22:47 -0500

    change

--- vue-node/src/service/auth.service.js
+++ vue-node/src/service/auth.service.js
@@ -2,35 +2,42 @@ import Cookies from "js-cookie";
 import { ApiRequest } from "@/service/index";
 
 const login = (user, pass) => {
-    return ApiRequest.post(
-        "/users/login",
-        {
-            email: user,
-            password: pass,
-        })
-}
+  return ApiRequest.post("/users/login", {
+    email: user,
+    password: pass,
+  });
+};
 const get = () => {
-    return ApiRequest.get(
-        "/users",
-    )
-}
+  return ApiRequest.get("/users");
+};
 
-const setAccessToken = (userLogged) => {
-    Cookies.set("userLogged", userLogged,)
-}
+const register = (nameUser, lastname, email, pass, confirmPass) => {
+  return ApiRequest.post("/users/register", {
+    nameUser,
+    lastname,
+    email,
+    pass,
+    confirmPass,
+  });
+};
+
+const setAccessToken = userLogged => {
+  Cookies.set("userLogged", userLogged);
+};
 
 const getAccessToken = () => {
-    return Cookies.get("userLogged");
-}
+  return Cookies.get("userLogged");
+};
 
 const deleteAccessToken = () => {
-    Cookies.remove('userLogged');
-}
+  Cookies.remove("userLogged");
+};
 
 export default {
-    login,
-    get,
-    setAccessToken,
-    getAccessToken,
-    deleteAccessToken
-}
+  login,
+  register,
+  get,
+  setAccessToken,
+  getAccessToken,
+  deleteAccessToken,
+};
