a43204030f88a1c611068b83319965d065d3183e
Yenifer Ramírez <yeniferramirez11@gmail.com>
2022-08-17 11:01:25 -0400

    Estrctura base para master

--- vue-node/README.md
+++ vue-node/README.md
@@ -22,6 +22,3 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
-
-WireFrame Figma
-https://www.figma.com/file/DG9GnIsAu7DzHoGLoJBYSO/Mundo-Mascota?node-id=0%3A1
--- vue-node/src/views/LoginView.vue
+++ vue-node/src/views/LoginView.vue
@@ -1,56 +1,13 @@
 <template>
-  <div>
-    <form @submit.prevent="handleSubmit">
-      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
-      <div class="form-floating">
-        <input
-          type="email"
-          class="form-control"
-          name="email"
-          placeholder="name@example.com"
-          v-model="email"
-        />
-        <label>Email</label>
-      </div>
-
-      <div class="form-floating">
-        <input
-          type="password"
-          class="form-control"
-          name="password"
-          placeholder="Password"
-          v-model="password"
-        />
-        <label>Password</label>
-      </div>
-
-      <button class="w-100 btn btn-lg btn-primary">Submit</button>
-    </form>
+  <div class="home">
+    <h1>Aca hacer el Login</h1>
   </div>
 </template>
 
 <script>
-import AuthService from "@/service/auth.service";
 export default {
   name: "LoginView",
-  data() {
-    return {
-      email: "",
-      password: "",
-    };
-  },
-  methods: {
-    handleSubmit() {
-      AuthService.login(this.email, this.password)
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-  },
+  data() {},
 };
 </script>
 
