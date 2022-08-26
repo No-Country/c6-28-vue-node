ae7d995141b0f23f28524e624d0770f5dd9f4365
Daniel Aguilar <dyangeltk17@gmail.com>
2022-08-26 17:54:20 -0500

    Add changes

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,37 +1,31 @@
 <template>
-    <div class="container rounded my-5">
-      <div class="row g-3">
-        <div class="col-md-12 col-lg-12 col-xl-12 align-items-center">
-          
-          <div class="align-items-center">
-            <img
-              class="card-img"
-              style="width: 28rem"
-              src=""
-            />
-            <img 
+  <div class="container rounded my-5">
+    <div class="row g-3">
+      <div class="col-md-12 col-lg-12 col-xl-12 align-items-center">
+        <div class="align-items-center">
+          <img class="card-img" style="width: 28rem" src="" />
+          <img
             class="card-img"
-              style="width: 28rem"
-              alt="Producto"
-          src="https://res.cloudinary.com/yeniferramirez11/image/upload/v1632962216/proyecto-grado/404_b7kd9z.gif"
-          height="400px"
+            style="width: 28rem"
+            alt="Producto"
+            src="https://res.cloudinary.com/yeniferramirez11/image/upload/v1632962216/proyecto-grado/404_b7kd9z.gif"
+            height="400px"
           />
-          </div>
         </div>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'NotFound'
-}
+  name: "NotFound",
+};
 </script>
 
 <style scoped>
-  .max {
-    max-width: 1200px;
-    margin: auto;
-  }
+.max {
+  max-width: 1200px;
+  margin: auto;
+}
 </style>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,8 +1,8 @@
 <script setup>
 import ButtonGoogle from "@/Components/ui/Button/ButtonGoogle.vue";
 import ButtonBase from "@/Components/ui/Button/ButtonBase.vue";
-import HeaderItem from "@/Components/HeaderItem.vue";
-import FooterView from "../Components/FooterView.vue";
+// import HeaderItem from "@/Components/HeaderItem.vue";
+// import FooterView from "../Components/FooterView.vue";
 import { ref } from "vue";
 
 const name = ref("");
@@ -12,7 +12,11 @@ const confirmPass = ref("");
 const email = ref("");
 </script>
 <template>
-  <form action="" class="register__form bg-white" @submit.prevent="handleSubmit">
+  <form
+    action=""
+    class="register__form bg-white"
+    @submit.prevent="handleSubmit"
+  >
     <h1 class="mb-5">Registro</h1>
     <div class="row mb-3">
       <div class="col">
